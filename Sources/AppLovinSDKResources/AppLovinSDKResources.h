bdf2ee5ee6417b607da82e9ba2fc5ccc3023d5cb
Ritam Sarmah <ritam.sarmah@applovin.com>
2024-08-02 16:30:22 -0700

    Add empty file

---
+++ Sources/AppLovinSDKResources/AppLovinSDKResources.h
@@ -0,0 +1,9 @@
+//
+//  AppLovinSDKResources.h
+//  AppLovinSDK
+//
+//  Created by Ritam Sarmah on 8/2/24.
+//  Copyright © 2024 AppLovin Corporation. All rights reserved.
+//
+
+// Empty file for SPM wrapper target
